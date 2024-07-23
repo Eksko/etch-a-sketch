ab2d22c76088267e5bddef1590a9fbb1f9dbaa7f
esko <esko.voobus@protonmail.com>
2024-07-23 23:20:36 +0300

    feat: generate 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello there!")
+const container = document.querySelector(".container")
+
+for (let i = 0; i < 16 * 16; i++) {
+  const div = document.createElement("div")
+  div.setAttribute("class", "pixel")
+  container.appendChild(div)
+}
+
+// -- vim: ts=2 sts=2 sw=2 et
