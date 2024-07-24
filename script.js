221d41efbe5fdbd22737e687fcc6365ef70823c3
esko <esko.voobus@protonmail.com>
2024-07-24 17:42:33 +0300

    feat: button for alternative grid size

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const container = document.querySelector(".container")
+const button = document.querySelector(".button")
 
-for (let i = 0; i < 16 * 16; i++) {
-  const div = document.createElement("div")
-  div.setAttribute("class", "pixel")
-  container.appendChild(div)
-}
+generateGrid()
 
 container.addEventListener("mouseover", e => {
   const pixel = e.target
   pixel.style.backgroundColor = "brown"
 })
+
+button.addEventListener("click", () => {
+  const input = prompt("1-100")
+  container.innerHTML = ""
+  generateGrid(input)
+})
+
+function generateGrid(n = 16) {
+  for (let i = 0; i < n * n; i++) {
+    const div = document.createElement("div")
+    div.setAttribute("class", "pixel")
+    div.style.width = `calc(100% / ${n})`
+    div.style.height = `calc(100% / ${n})`
+    container.appendChild(div)
+  }
+}
 
 // -- vim: ts=2 sts=2 sw=2 et
